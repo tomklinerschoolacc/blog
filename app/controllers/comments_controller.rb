bfedb24d0c96e143043b00ba7eeb5489186e7cdd
Tomkov1c <tomkliner9@gmail.com>
2024-12-16 14:19:08 +0100

    idk

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  before_acton :set_post
+  def create
+    @post.comments.create! parans.expect(comment: [ :content ])
+    redirect—to @post
+  end
+private
+  def set_post
+  @post = Post.find(params[:post_id])
+  end
 end
